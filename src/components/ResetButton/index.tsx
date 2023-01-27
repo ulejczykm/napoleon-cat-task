4b6af1e79096291be0ae07599d172a29c1ee8149
Mariusz Ulejczyk <ulejczykm@gmail.com>
2023-01-27 19:11:17 +0100

    create ResetButton component with styles

---
+++ src/components/ResetButton/index.tsx
@@ -0,0 +1,24 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import "./style.css";
+
+interface Props {
+  setReset: Dispatch<SetStateAction<boolean>>;
+}
+
+export const ResetButton = ({ setReset }: Props) => {
+  const [buttonPress, setButtonPress] = useState<boolean>(false);
+
+  return (
+    <div className="buttonWrapper">
+      <button
+        style={{ backgroundColor: buttonPress ? "#3A9F9F" : "#30CCCC" }}
+        onMouseDown={() => setButtonPress(true)}
+        onMouseUp={() => setButtonPress(false)}
+        onClick={() => setReset(true)}
+        className="button"
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
